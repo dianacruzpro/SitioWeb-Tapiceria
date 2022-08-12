d3a490d9a68697c31a0748159d0434d78be8b1c8
Diana Cruz <saravcruz1501@gmail.com>
2022-08-11 19:26:32 -0600

    removing comments

--- administrador/config/bd.php
+++ administrador/config/bd.php
@@ -6,10 +6,6 @@
 
   try{
     $conexion = new PDO("mysql:host=$host;dbname=$bd",$usuario,$password);
-    // if($conexion){echo "Conectado.... a sistema <br>";}
-    // echo "Base ".$bd."<br>";
-    // echo "Usuario ".$usuario."<br>";
-    // echo "host ".$host."<br>";
   }
   catch(Exception $ex){
     echo $ex->getMessage();
